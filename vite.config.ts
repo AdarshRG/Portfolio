d936fbebd77cc27324697b98393812ccae064f6a
Adarsh RG <adarshrg1998@gmail.com>
2025-05-10 23:56:43 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base:"/Portfolio",
+  base:"/Portfolio/",
   plugins: [
     react(),
     mode === 'development' &&
